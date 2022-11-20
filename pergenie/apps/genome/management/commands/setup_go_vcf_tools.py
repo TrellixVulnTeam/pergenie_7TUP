dfa9b7c69485962450562f0ff3a7e97c44dc81b9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-20 12:43:33 +0000

    Adding tarfile member sanitization to extractall()

--- pergenie/apps/genome/management/commands/setup_go_vcf_tools.py
+++ pergenie/apps/genome/management/commands/setup_go_vcf_tools.py
@@ -29,7 +29,26 @@ class Command(BaseCommand):
         log.info('Extracting go-vcf-tools ...')
         with tarfile.open(tar_gz, 'r') as tar:
             dst = os.path.join(tmp_dir, 'go-vcf')
-            tar.extractall(dst)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, dst)
             for tool in glob.glob(os.path.join(tmp_dir, 'go-vcf', '*')):
                 shutil.copy(tool, bin_dir)
 
